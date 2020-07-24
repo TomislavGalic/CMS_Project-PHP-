cfe2e972d4dcc12d829348985be12d46e272263b
Tomislav Galić <tomislav.galic19@gmail.com>
2020-07-24 14:14:35 +0200

    testing

--- www/article.php
+++ www/article.php
@@ -2,42 +2,42 @@
 
 require 'includes/database.php';
 
+$conn = getDB();
+
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 
+    $sql = "SELECT *
+            FROM article
+            WHERE id = " . $_GET['id'];
 
-$sql = "SELECT *
-        FROM article
-        WHERE id = " . $_GET['id'];
+    $results = mysqli_query($conn, $sql);
 
-$results = mysqli_query($conn, $sql);
+    if ($results === false) {
 
-if ($results === false) {
+        echo mysqli_error($conn);
 
-    echo mysqli_error($conn);
+    } else {
+
+        $article = mysqli_fetch_assoc($results);
+
+    }
 
 } else {
-
-    $article = mysqli_fetch_assoc($results);
-
-}
-
-} else {
-  $article = null;
+    $article = null;
 }
 
 ?>
-
 <?php require 'includes/header.php'; ?>
 
-        <?php if ($article === null): ?>
-            <p>Article not found.</p>
-        <?php else: ?>
+<?php if ($article === null): ?>
+    <p>Article not found.</p>
+<?php else: ?>
 
-            <article>
-                <h2><?= $article['title']; ?></h2>
-                <p><?= $article['content']; ?></p>
-            </article>
+    <article>
+        <h2><?= htmlspecialchars($article['title']); ?></h2>
+        <p><?= htmlspecialchars($article['content']); ?></p>
+    </article>
 
-        <?php endif; ?>
+<?php endif; ?>
 
 <?php require 'includes/footer.php'; ?>

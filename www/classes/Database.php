aae6e9dd78a95feea98fb510dd0f104077fd58ec
Tomislav Galić <tomislav.galic19@gmail.com>
2020-07-25 17:31:46 +0200

    test

---
+++ www/classes/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+*Database
+*
+*A connection to the database
+*/
+
+class Database {
+
+      /**
+      *Get the database connection
+      *
+      *@return PDO object Connection to the database server
+      *
+      **/
+
+  public getConn()
+  {
+    $db_host = "localhost";
+    $db_name = "cms";
+    $db_user = "cms_www";
+    $db_pass = "T2dZdug195iZdBY7";
+
+    $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
+
+    return new PDO($dsn, $db_user, $db_pass);
+
+  }
+}
